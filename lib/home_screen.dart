0e192acdca9fe0f3218aaf78ef755d54a4e524f2
aniketambore <aniketambore0@gmail.com>
2023-07-23 18:02:32 +0530

    Refresh Indicator for feeds

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
@@ -16,9 +17,24 @@ class _HomeScreenState extends State<HomeScreen> {
   bool _isConnected = false;
   final _relay = RelayApi(relayUrl: 'wss://relay.damus.io');
   final List<Event> _events = [];
+  late Stream<Event> _stream;
+  final _controller = StreamController<Event>();
 
-  Stream get relayStream async* {
+  @override
+  void initState() {
+    _initStream();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _relay.close();
+    super.dispose();
+  }
+
+  Future<Stream<Event>> _connectToRelay() async {
     final stream = await _relay.connect();
+
     _relay.on((event) {
       if (event == RelayEvent.connect) {
         setState(() => _isConnected = true);
@@ -26,6 +42,7 @@ class _HomeScreenState extends State<HomeScreen> {
         setState(() => _isConnected = false);
       }
     });
+
     _relay.sub([
       Filter(
         kinds: [1],
@@ -35,16 +52,28 @@ class _HomeScreenState extends State<HomeScreen> {
       )
     ]);
 
-    await for (var message in stream) {
-      if (message.type == 'EVENT') {
-        Event event = message.message;
-        if (event.kind == 1) {
-          _events.add(event);
-        }
-      }
+    return stream
+        .where((message) => message.type == 'EVENT')
+        .map((message) => message.message);
+  }
 
-      yield message;
-    }
+  void _initStream() async {
+    _stream = await _connectToRelay();
+    _stream.listen((event) {
+      if (event.kind == 1) {
+        setState(() => _events.add(event));
+      }
+      _controller.add(event);
+    });
+  }
+
+  Future<void> _resubscribeStream() async {
+    await Future.delayed(const Duration(seconds: 1), () {
+      setState(() {
+        _events.clear();
+      });
+      _initStream(); // Reconnect and resubscribe to the filter
+    });
   }
 
   @override
@@ -54,6 +83,14 @@ class _HomeScreenState extends State<HomeScreen> {
         title: const Text('Podcast Feed'),
         centerTitle: true,
         backgroundColor: _isConnected ? Colors.indigoAccent : Colors.redAccent,
+        actions: [
+          IconButton(
+            onPressed: () async {
+              await _resubscribeStream();
+            },
+            icon: const Icon(Icons.refresh_outlined),
+          )
+        ],
       ),
       body: _buildPodcastList(),
       floatingActionButton: FloatingActionButton(
@@ -68,25 +105,30 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   Widget _buildPodcastList() {
-    return StreamBuilder(
-      stream: relayStream,
-      builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          return ListView.builder(
-            itemCount: _events.length,
-            itemBuilder: (context, index) {
-              final event = _events[index];
-              return PodcastCard(podcastData: jsonDecode(event.content));
-              // return Text(event.content);
-            },
-          );
-        } else if (snapshot.connectionState == ConnectionState.waiting) {
-          return const Center(child: Text('Loading....'));
-        } else if (snapshot.hasError) {
-          return Center(child: Text('Error: ${snapshot.error}'));
-        }
-        return const Center(child: CircularProgressIndicator());
+    return RefreshIndicator(
+      onRefresh: () async {
+        await _resubscribeStream();
       },
+      child: StreamBuilder(
+        stream: _controller.stream,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder(
+              itemCount: _events.length,
+              itemBuilder: (context, index) {
+                final event = _events[index];
+                return PodcastCard(podcastData: jsonDecode(event.content));
+                // return Text(event.content);
+              },
+            );
+          } else if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: Text('Loading....'));
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          }
+          return const Center(child: CircularProgressIndicator());
+        },
+      ),
     );
   }
 
